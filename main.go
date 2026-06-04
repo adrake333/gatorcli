2edeb39e672d2a47e373eb1ace9b2be5735672cb
adrake333 <adrake333@gmail.com>
2026-06-04 15:27:12 -0500

    first test

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ package main
 
 
 import (
-	"os"
+	"fmt"
+	"github.com/adrake333/gatorcli/internal/config"
 )
 
 
 
 
 func main() {
-	
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cfg.SetUser("adam")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v", cfg)
 }
