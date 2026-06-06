c68ac9a2b27b9159ed6ab2d3e7d3a4943ba794f4
adrake333 <adrake333@gmail.com>
2026-06-06 18:33:22 -0500

    ready to test commands.go v1

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/adrake333/gatorcli/internal/config"
 	"log"
+	"os"
 )
 
 
@@ -17,13 +18,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = cfg.SetUser("adam")
-	if err != nil {
-		log.Fatal(err)
+	appState := state{
+		cfg: cfg,
 	}
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
+	cmds := commands{
+		handlers: make(map[string]func(*state, command) error),
 	}
-	fmt.Printf("%v", cfg)
+	cmds.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("invalid command")
+		os.Exit(1)
+	}
+	commandName := os.Args[1]
+	commandArgs := os.Args[2:]
+	cmd := command{
+		name: commandName,
+		args: commandArgs,
+	}
+	err = cmds.run(&appState, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
