d8e081e2f8956682d42993db1862f06df67cde1f
adrake333 <adrake333@gmail.com>
2026-06-06 10:37:12 -0500

    added commands.go

--- commands.go
+++ commands.go
@@ -4,5 +4,47 @@ package main
 
 
 import (
+	"errors"
+	"fmt"
 	"github.com/adrake333/gatorcli/internal/config"
 )
+
+
+
+
+type state struct {
+	cfg	*config.Config
+}
+
+type command struct {
+	name	string
+	args	[]string
+}
+
+type commands struct {
+	handlers	map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %v", cmd)
+	}
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("invalid command")
+	}
+	err := s.config.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println("user has been set")
+	return nil
+}
